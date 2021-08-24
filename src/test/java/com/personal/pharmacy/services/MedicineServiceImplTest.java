f393c464825cf45c5c890bf95d3470f26afdaa17
ravinder sian <rsian761@gmail.com>
2021-08-24 18:50:48 +0100

    remove pointless tests

--- src/test/java/com/personal/pharmacy/services/MedicineServiceImplTest.java
+++ src/test/java/com/personal/pharmacy/services/MedicineServiceImplTest.java
@@ -1,11 +1,7 @@
 package com.personal.pharmacy.services;
 
-import static org.mockito.Mockito.times;
-import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Optional;
 
 import org.junit.jupiter.api.Assertions;
@@ -34,63 +30,7 @@ class MedicineServiceImplTest {
 	}
 
 	@Test
-	public void test_Save_CallsRepositorySave_WhenCalled() {
-		//Act
-		medicineService.save(medicineMock);
-		//Assert
-		verify(medicineRepository, times(1)).save(medicineMock);
-	}
-	
-	@Test
-	public void test_Save_ReturnsCorrectMedicine_WhenGivenMedicineMock() {
-		//Arrange
-		when(medicineRepository.save(medicineMock)).thenReturn(medicineMock);
-		//Assert
-		Assertions.assertEquals(medicineMock, medicineService.save(medicineMock));
-	}
-	
-	@Test
-	public void test_Delete_CallsRepositoryDelete_WhenCalled() {
-		//Act
-		medicineService.delete(medicineMock);
-		//Assert
-		verify(medicineRepository, times(1)).delete(medicineMock);
-	}
-	
-	@Test
-	public void test_FindAll_Calls_RepositoryFindAll_WhenCalled() {
-		//Act
-		medicineService.findAll();
-		//Assert
-		verify(medicineRepository, times(1)).findAll();
-	}
-
-	@Test
-	public void test_FindAll_ReturnsEmptyList_WhenGivenNothing() {
-		//Arrange
-		when(medicineRepository.findAll()).thenReturn(new ArrayList<>());
-		//Assert
-		Assertions.assertEquals(0, medicineService.findAll().size());
-	}
-	
-	@Test
-	public void test_FindAll_ReturnsListOfSize1_WhenGivenMedicineMock() {
-		//Arrange
-		when(medicineRepository.findAll()).thenReturn(new ArrayList<Medicine>(Arrays.asList(medicineMock)));
-		//Assert
-		Assertions.assertEquals(1, medicineService.findAll().size());
-	}
-	
-	@Test
-	public void test_FindAll_ReturnsListOfSize2_WhenGivenDuplicateMedicineMock() {
-		//Arrange
-		when(medicineRepository.findAll()).thenReturn(new ArrayList<Medicine>(Arrays.asList(medicineMock, medicineMock)));
-		//Assert
-		Assertions.assertEquals(2, medicineService.findAll().size());
-	}
-	
-	@Test
-	public void test_FindById_ReturnsMedicineMock_WhenCalledWithId1() {
+	void test_FindById_ReturnsMedicineMock_WhenCalledWithId1() {
 		//Arrange
 		when(medicineRepository.findById(1L)).thenReturn(Optional.of(medicineMock));
 		//Assert
@@ -98,13 +38,13 @@ class MedicineServiceImplTest {
 	}
 	
 	@Test
-	public void test_FindById_ReturnsEmptyOptional_WhenCalledWithId5() {
+	void test_FindById_ReturnsEmptyOptional_WhenCalledWithId5() {
 		//Assert
 		Assertions.assertTrue(medicineService.findById(5L).isEmpty());
 	}
 	
 	@Test
-	public void test_UpdateName_ReturnsMedicineWithCorrectName_WhenGivenStringName() {
+	void test_UpdateName_ReturnsMedicineWithCorrectName_WhenGivenStringName() {
 		//Arrange
 		Medicine medicine = new Medicine();
 		medicine.setName("mock");
