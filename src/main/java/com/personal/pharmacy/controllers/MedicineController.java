c4bd8325e8353bb49ff1a15e92bd8eb81439b01f
ravinder sian <rsian761@gmail.com>
2021-09-08 23:40:51 +0100

    remove brackets on field errors

--- src/main/java/com/personal/pharmacy/controllers/MedicineController.java
+++ src/main/java/com/personal/pharmacy/controllers/MedicineController.java
@@ -1,7 +1,7 @@
 package com.personal.pharmacy.controllers;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Optional;
 
 import org.springframework.http.HttpStatus;
@@ -50,12 +50,9 @@ public class MedicineController implements CrudController<Medicine, Long>{
 	@Override
 	public ResponseEntity<?> add(Medicine medicine, BindingResult bindingResult){
 		if (bindingResult.hasFieldErrors()) {
-			List<String> errorStrings = new ArrayList<>();
-			bindingResult.getFieldErrors().forEach(objectError -> {
-				errorStrings.add(objectError.getDefaultMessage());
-				
-			});
-			return new ResponseEntity<>(errorStrings.toString(), HttpStatus.BAD_REQUEST);
+			Map<String, String> errors = new HashMap<>();
+			bindingResult.getFieldErrors().forEach(error -> errors.put(error.getField(), error.getDefaultMessage()));
+			return new ResponseEntity<>(errors, HttpStatus.BAD_REQUEST);
 		}
 		Medicine savedMedicine = medicineService.save(medicine);
 		return new ResponseEntity<>(savedMedicine, HttpStatus.OK);
